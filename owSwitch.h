9665faf0568c6f65082cb1de615bface97888f7b
Andrey Klimov <anklimov@gmail.com>
2017-10-02 15:15:04 +0300

    Apache 2.0 License added

--- owSwitch.h
+++ owSwitch.h
@@ -1,3 +1,23 @@
+/* Copyright © 2017 Andrey Klimov. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Homepage: http://lazyhome.ru
+GIT:      https://github.com/anklimov/lighthub
+e-mail    anklimov@gmail.com
+
+*/
+
 //define APU_OFF
 #include <OneWire.h>
 #include <DallasTemperature.h>
