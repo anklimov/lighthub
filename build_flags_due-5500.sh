41df3b701e5f549e8211db585079288eaf55c637
livello <livello@bk.ru>
2018-10-15 21:27:58 +0300

    remove build_flags

--- build_flags_due-5500.sh
+++ build_flags_due-5500.sh
@@ -1,31 +1,29 @@
 #! /bin/bash
 # usage:
 # first make your own copy of template
-# cp build_flags_template.sh build_flags_ENVNAME.sh
+# cp build_flags_template.sh my_build_flags.sh
 # then edit, change or comment something
- export FLAGS="-DMY_CONFIG_SERVER=lazyhome.ru"
- #export FLAGS="$FLAGS -DWATCH_DOG_TICKER_DISABLE"
+# nano  my_build_flags.sh
+# and source it
+# source my_build_flags.sh
+ export FLAGS="-DMY_CONFIG_SERVER=lighthub.elistech.ru"
+ export FLAGS="$FLAGS -DWATCH_DOG_TICKER_DISABLE"
  #export FLAGS="$FLAGS -DUSE_1W_PIN=12"
  #export FLAGS="$FLAGS -DSD_CARD_INSERTED"
  export FLAGS="$FLAGS -DSERIAL_BAUD=115200"
  export FLAGS="$FLAGS -DWiz5500"
- #export FLAGS="$FLAGS -DDISABLE_FREERAM_PRINT"
- export FLAGS="$FLAGS -DCUSTOM_FIRMWARE_MAC=de:ad:be:ef:fe:ff"
+ export FLAGS="$FLAGS -DDISABLE_FREERAM_PRINT"
+ export FLAGS="$FLAGS -DCUSTOM_FIRMWARE_MAC=C2:3E:1f:03:1B:1E"
 # export FLAGS="$FLAGS -DDMX_DISABLE"
-# export FLAGS="$FLAGS -DARTNET_ENABLE"
 # export FLAGS="$FLAGS -DMODBUS_DISABLE"
 # export FLAGS="$FLAGS -DOWIRE_DISABLE"
-# export FLAGS="$FLAGS -DAVR_DMXOUT_PIN=18"
-# export FLAGS="$FLAGS -DLAN_INIT_DELAY=2000"
-# export FLAGS="$FLAGS -DCONTROLLINO"
-# export FLAGS="$FLAGS -DESP_WIFI_AP=MYAP"
-# export FLAGS="$FLAGS -DESP_WIFI_PWD=MYPWD"
-# export FLAGS="$FLAGS -DWIFI_MANAGER_DISABLE"
-# export FLAGS="$FLAGS -DDHT_DISABLE"
-# export FLAGS="$FLAGS -DRESET_PIN=5"
-# export FLAGS="$FLAGS -DDHCP_RETRY_INTERVAL=60000"
-# export FLAGS="$FLAGS -DRESTART_LAN_ON_MQTT_ERRORS"
-# export FLAGS="$FLAGS -DW5500_CS_PIN=53"
- export FLAGS="$FLAGS -DWITH_STREAMING_LIB"
+ #export FLAGS="$FLAGS -DAVR_DMXOUT_PIN=18"
+ #export FLAGS="$FLAGS -DCONTROLLINO"
+# export FLAGS="$FLAGS -DRESET_PIN=8"
+ export FLAGS="$FLAGS -DLAN_INIT_DELAY=2000"
+# export FLAGS="$FLAGS -DESP_WIFI_AP=vent"
+# export FLAGS="$FLAGS -DESP_WIFI_PWD=kk007remont"
+# export FLAGS="$FLAGS -DSYSLOG_DISABLE"
+ export FLAGS="$FLAGS  -std=gnu++11"
  export FLAGS="$FLAGS -DPIO_SRC_REV="$(git log --pretty=format:%h_%ad -1 --date=short)
  echo $FLAGS
