b691c137f9f1c4e4a349cd990941517f031ea17c
Sergey Konyukhov <live110.sk@gmail.com>
2019-01-30 16:17:39 +0300

    fixed esp8266 compilation without custom build flags

--- check_custom_build_flags_esp8266.sh
+++ check_custom_build_flags_esp8266.sh
@@ -3,4 +3,6 @@ CUSTOM_BUILD_FLAGS_FILE=custom-build-flags/build_flags_esp8266.sh
 if [ -f $CUSTOM_BUILD_FLAGS_FILE ]; then
     source $CUSTOM_BUILD_FLAGS_FILE
 fi
+export FLAGS="$FLAGS -DDMX_DISABLE"
+export FLAGS="$FLAGS -DMODBUS_DISABLE"
  echo $FLAGS
