9afef45a2ad9bfe63438f42dcbb0b24a92a1ba5d
Andrey Klimov <anklimov@gmail.com>
2020-12-19 01:11:47 +0300

    OTA tools for win added

--- compiled/lighthub21/uploadHello.sh
+++
@@ -1,1 +0,0 @@
-../tools/mac/arduinoOTA -address 192.168.88.59 -port 65280 -username arduino -password password -sketch hello.bin -b -upload /sketch
---
+++ compiled/lighthub21/uploadOTA.bat
@@ -0,0 +1,1 @@
+..\tools\win\arduinoOTA.exe -address 192.168.88.21 -port 65280 -username arduino -password password -sketch firmware.bin -b -upload /sketch
